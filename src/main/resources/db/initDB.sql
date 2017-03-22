67044502ccf2fd0db9c6fb6fc587e5b8508bbb34
Kirill Frolov <kilfro@hotmail.com>
2017-03-23 02:26:34 +0300

    add SQL scripts - init & populate - for meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,20 +1,23 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
 CREATE TABLE users
 (
-  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name       VARCHAR NOT NULL,
-  email      VARCHAR NOT NULL,
-  password   VARCHAR NOT NULL,
-  registered TIMESTAMP DEFAULT now(),
-  enabled    BOOL DEFAULT TRUE,
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR              NOT NULL,
+  email            VARCHAR              NOT NULL,
+  password         VARCHAR              NOT NULL,
+  registered       TIMESTAMP           DEFAULT now(),
+  enabled          BOOL                DEFAULT TRUE,
   calories_per_day INTEGER DEFAULT 2000 NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -23,3 +26,14 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE public.meals
+(
+  id INTEGER DEFAULT nextval('global_seq') PRIMARY KEY,
+  description TEXT NOT NULL,
+  calories INTEGER NOT NULL,
+  date TIMESTAMP DEFAULT now(),
+  user_id INTEGER NOT NULL,
+  CONSTRAINT meals_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE UNIQUE INDEX meals_id_uindex ON public.meals (id);
